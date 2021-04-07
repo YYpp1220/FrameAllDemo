ff581eda824ae76fdf1ea7aff3f75e46aee906d8
diaojiahuitest <diaojiahuitest@qq.com>
2021-04-07 18:15:14 +0800

    SpringCloud Alibaba Nacos Demo

--- nacos-discovery/nacos-restful-consumer/src/main/java/com/djh/consumer/RestConsumerApplication.java
+++ nacos-discovery/nacos-restful-consumer/src/main/java/com/djh/consumer/RestConsumerApplication.java
@@ -2,6 +2,7 @@ package com.djh.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
  * @date 2021/04/02
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RestConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestConsumerApplication.class, args);
--- nacos-discovery/nacos-restful-consumer/src/main/java/com/djh/consumer/controller/RestConsumerController.java
+++ nacos-discovery/nacos-restful-consumer/src/main/java/com/djh/consumer/controller/RestConsumerController.java
@@ -2,9 +2,13 @@ package com.djh.consumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 /**
  * 其他消费者控制器
@@ -17,6 +21,15 @@ public class RestConsumerController {
     @Autowired(required = false)
     private RestTemplate restTemplate;
 
+    /**
+     *
+     *  服务id即注册中心的中的服务名
+     */
+    private static final String NACOS_SERVICE_ID="nacos-restful-provider";
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
     @Value("${provider.address}")
     private String providerAddress;
 
@@ -26,7 +39,10 @@ public class RestConsumerController {
 
     @GetMapping("/service")
     public String service () {
-        String providerStr = restTemplate.getForObject(HTTP_TOP + providerAddress + HTTP_TAIL, String.class);
+        // String providerStr = restTemplate.getForObject(HTTP_TOP + providerAddress + HTTP_TAIL, String.class);
+        ServiceInstance serviceInstance = loadBalancerClient.choose(NACOS_SERVICE_ID);
+        URI uri = serviceInstance.getUri();
+        String providerStr = restTemplate.getForObject(uri + HTTP_TAIL, String.class);
         System.out.println("consumer | invoke" + providerStr);
         return "consumer | invoke" + providerStr;
     }
